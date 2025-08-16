55bcd92cfbd09ae7e0fba8b52f6b9427b2c10c19
miskatormi <miska.marius@gmail.com>
2025-08-16 21:05:44 +0300

    s

--- GBM.py
+++ GBM.py
@@ -1,1 +1,9 @@
 
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from core_lib.core_fdm import SDEModel, EulerMaruyamaMonteCarlo, MonteCarloSummary, SensitivitySDEAnalysisTool, Calibrator
+
+# A weekly simple forecast with Geometric Brownian Motion for AAPL
+
+pd.read_csv('FinancialDEModels\AAPL_6M.csv', )
